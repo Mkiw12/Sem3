d397fd3fb29ab7345519ea210321bb7d6e200af0
jockipocky <jacob.j.emanuelsson@gmail.com>
2024-04-26 09:40:13 +0200

    test

---
+++ src/main/integration/SalesLog.java
@@ -0,0 +1,5 @@
+package main.integration;
+
+public class SalesLog {
+    
+}
--- src/main/view/view.java
+++ src/main/view/view.java
@@ -1,1 +1,14 @@
 package main.view;
+
+import main.controller.Controller;
+
+
+public class View
+{
+    private Controller control;
+
+    public View(Controller control)
+    {
+        this.control = control;
+    }
+}
